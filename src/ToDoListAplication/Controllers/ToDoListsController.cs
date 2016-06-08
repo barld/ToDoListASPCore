140d4fbf25fb9050764640c207d2635a67e7e3f9
barld <bakeds@gmail.com>
2016-06-08 15:48:10 +0200

    first version comfirm all requirments

--- src/ToDoListAplication/Controllers/ToDoListsController.cs
+++ src/ToDoListAplication/Controllers/ToDoListsController.cs
@@ -10,6 +10,8 @@ using ToDoListAplication.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
+using ToDoListAplication.Models.ViewModels;
+
 namespace ToDoListAplication.Controllers
 {
     [Authorize]
@@ -27,32 +29,14 @@ namespace ToDoListAplication.Controllers
         // GET: ToDoLists
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.ToDoList.Include(t => t.Owner);
+            var applicationDbContext = _context.ToDoList
+                .Include(t => t.Owner)
+                .Where(t => t.ApplicationUserId == _userManager.GetUserId(User));
             return View(await applicationDbContext.ToListAsync());
         }
-
-        // GET: ToDoLists/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var toDoList = await _context.ToDoList.SingleOrDefaultAsync(m => m.Id == id);
-            if (toDoList == null)
-            {
-                return NotFound();
-            }
-
-            return View(toDoList);
-        }
-
         // GET: ToDoLists/Create
         public IActionResult Create()
         {
-            var users = _context.Users.Select(u => new { Id = u.Id, Owner = u.UserName });
-            ViewData["ApplicationUserId"] = new SelectList(users, "Id", "Owner");
             return View();
         }
 
@@ -61,16 +45,23 @@ namespace ToDoListAplication.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ApplicationUserId,Name")] ToDoList toDoList)
+        public async Task<IActionResult> Create(Models.ViewModels.CreatToDoList create)
         {
             if (ModelState.IsValid)
             {
+                var toDoList = new ToDoList()
+                {
+                    ApplicationUserId = _userManager.GetUserId(User),
+                    Name = create.Name,
+                    Tasks = create.Tasks
+                        .Where(s => !string.IsNullOrEmpty(s))
+                        .Select(s => new ToDoTasks() { Done = false,Task = s}).ToList()
+                };
                 _context.Add(toDoList);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Owner", toDoList.ApplicationUserId);
-            return View(toDoList);
+            return View(create);
         }
 
         // GET: ToDoLists/Edit/5
@@ -81,13 +72,15 @@ namespace ToDoListAplication.Controllers
                 return NotFound();
             }
 
-            var toDoList = await _context.ToDoList.SingleOrDefaultAsync(m => m.Id == id);
+            var toDoList = await _context.ToDoList.Include(m => m.Tasks).SingleOrDefaultAsync(m => m.Id == id && m.ApplicationUserId == _userManager.GetUserId(User));
             if (toDoList == null)
             {
                 return NotFound();
             }
-            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Owner", toDoList.ApplicationUserId);
-            return View(toDoList);
+            var viewModel = new EditToDoList() { Id = toDoList.Id, Name = toDoList.Name, Tasks = toDoList.Tasks.ToList() };
+            viewModel.Tasks.Add(new ToDoTasks());
+            viewModel.Tasks.Add(new ToDoTasks());
+            return View(viewModel);
         }
 
         // POST: ToDoLists/Edit/5
@@ -95,34 +88,60 @@ namespace ToDoListAplication.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ApplicationUserId,Name")] ToDoList toDoList)
+        public async Task<IActionResult> Edit(int id, EditToDoList toDoList)
         {
             if (id != toDoList.Id)
             {
                 return NotFound();
-            }
+            }          
 
             if (ModelState.IsValid)
             {
-                try
+                var origenal = await _context.ToDoList.Include(tl => tl.Tasks)
+                .FirstOrDefaultAsync(tl => tl.Id == id && tl.ApplicationUserId == _userManager.GetUserId(User));
+
+                if(origenal != null)
                 {
-                    _context.Update(toDoList);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ToDoListExists(toDoList.Id))
+                    var newTaks = toDoList.Tasks.Where(nt => !string.IsNullOrWhiteSpace(nt.Task)).ToList();
+
+                    origenal.Name = toDoList.Name;
+                    
+                    var tasks = origenal.Tasks.ToList()
+                        .Where(t => newTaks.Exists(nt => nt.Id == t.Id)).ToList();
+                    tasks.ForEach(t =>
+                        {
+                            var newtask = newTaks.Find(nt => nt.Id == nt.Id);
+                            t.Done = newtask.Done;
+                            t.Task = newtask.Task;
+                            //_context.Entry(t).State = EntityState.Modified;
+                            //_context.Update(t);
+                        }
+                    );
+                    origenal.Tasks = tasks.ToList();
+
+
+                    try
                     {
-                        return NotFound();
+                        _context.Entry(origenal).State = EntityState.Modified;
+                        _context.Update(origenal);
+                        await _context.SaveChangesAsync();
                     }
-                    else
+                    catch (DbUpdateConcurrencyException)
                     {
-                        throw;
+                        if (!ToDoListExists(toDoList.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
+                    return RedirectToAction("Index");
                 }
-                return RedirectToAction("Index");
+
+                
             }
-            ViewData["ApplicationUserId"] = new SelectList(_context.Users, "Id", "Owner", toDoList.ApplicationUserId);
             return View(toDoList);
         }
 
@@ -139,7 +158,6 @@ namespace ToDoListAplication.Controllers
             {
                 return NotFound();
             }
-
             return View(toDoList);
         }
 
---
+++ src/ToDoListAplication/Models/ViewModels/EditToDoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListAplication.Models.ViewModels
+{
+    public class EditToDoList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ToDoTasks> Tasks { get; set; }
+    }
+}
